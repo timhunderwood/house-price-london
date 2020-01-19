214f5cfe438802d8ba9527ffa5eb9c311ff98c6f
tim <43515959+timhunderwood@users.noreply.github.com>
2020-01-19 16:16:27 +0000

    Many docstrings and types and pipenv.

--- borough_map/data_loader.py
+++ borough_map/data_loader.py
@@ -1,38 +1,50 @@
-import pandas as pd
-import numpy as np
 import os
 import logging
+from typing import Optional, List, Tuple
+
+import pandas
+import numpy
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__file__)
 LOGGER.setLevel("DEBUG")
-LOGGER.info("data loader")
-LOGGER.debug("data loader")
 
 
-class DataLoader(object):
-    """
-    Responsible for loading aggregating and analysing the data set
+class DataLoader:
+    """Responsible for loading aggregating and analysing the data set.
+
+    Reads the raw data file of Price paid data UK and creates an aggregated file
+    that has all the data required for plotting. If it finds this file in future runs
+    it does not read the raw file again (caching).
+
+    The public method load_prepare_and_aggregate_data should be called once after instantiation
+    to load the data frame. Then public methods can be called on the object to get data.
     """
 
     def __init__(self):
-        self._raw_df = None
+        """Instantiate the DataLoader.
+
+        All dataframes are initialised as None and then populated by calling load_prepare_and_aggregate_data.
+        """
+        self._raw_df: Optional[pandas.DataFrame] = None
+        self._borough_data: Optional[pandas.DataFrame] = None
+        self._aggregated_data: Optional[pandas.DataFrame] = None
+
         self.all_london_boroughs = self.get_all_london_boroughs()
-        self.max_price = 100e6
-        self._data_directory = os.path.join(os.getcwd(), "..", "data")
-        self._cached_data_path = os.path.join(
+        self.max_price: int = 100e6
+        self._data_directory: str = os.path.join(os.getcwd(), "..", "data")
+        self._cached_data_path: str = os.path.join(
             self._data_directory, "london_aggregated_cache.csv"
         )
-        self._cached_yearly_data_path = os.path.join(
+        self._cached_yearly_data_path: str = os.path.join(
             self._data_directory, "yearly_london_aggregated_cache.csv"
         )
-        self.default_data_path = os.path.join(
+        self.default_data_path: str = os.path.join(
             self._data_directory, "pp-complete_20190112.csv.gz"
         )
 
-    def get_all_london_boroughs(self):
-        """
-        Returns list of all london boroughs in upper case
+    def get_all_london_boroughs(self) -> List[str]:
+        """Returns list of all london boroughs in upper case
 
         :return list(str): list of all london boroughs in upper case
         """
@@ -74,16 +86,16 @@ class DataLoader(object):
             "Sutton",
             "Waltham Forest",
         ]
-        inner_london_boroughs = list(map(str.upper, inner_london_boroughs))
-        outer_london_boroughs = list(map(str.upper, outer_london_boroughs))
-        return inner_london_boroughs + outer_london_boroughs
+        return [str.upper(borough) for borough in inner_london_boroughs+outer_london_boroughs ]
 
-    def _load_data(self, path):
-        """
-        Reads the data from CSV and loads it into a dataframe. The private _data attribute is populated with the result.
+
+    def _load_data(self, path: str) -> pandas.DataFrame:
+        """Reads the data from CSV and loads it into a dataframe.
+
+        The private _data attribute is populated with the result.
 
         :param path:
-        :return None:
+        :return: the raw full data frame of price paid data UK
         """
         names = [
             "transaction_id",
@@ -105,7 +117,7 @@ class DataLoader(object):
         ]
         dtypes = [
             str,
-            np.int64,
+            numpy.int64,
             str,
             str,
             "category",
@@ -124,7 +136,7 @@ class DataLoader(object):
         use_cols = [0, 1, 2, 3, 4, 5, 6, 12]
         use_names = [names[i] for i in use_cols]
         use_dtypes = {names[i]: dtypes[i] for i in use_cols}
-        raw_df = pd.read_csv(
+        raw_df = pandas.read_csv(
             path,
             delimiter=",",
             names=use_names,
@@ -134,17 +146,16 @@ class DataLoader(object):
             engine="c",
             compression="gzip",
         )
-        self._raw_df = raw_df
         LOGGER.debug("completed reading raw data")
+        return raw_df
 
-    def _update_data_for_london_analysis(self):
-        """
-        Updates the _data DataFrame to only include london boroughs
+    def _update_data_for_london_analysis(self) -> pandas.DataFrame:
+        """Updates the _data DataFrame to only include london boroughs.
 
-        :return:
+        :return: the raw dataframe but filtered to only london boroughs
         """
         df = self._raw_df.copy()
-        df["date_time"] = pd.to_datetime(df["date_time"])
+        df["date_time"] = pandas.to_datetime(df["date_time"])
         df["year"] = df["date_time"].dt.year
         df["month"] = df["date_time"].dt.month
         df["day"] = df["date_time"].dt.day
@@ -154,23 +165,22 @@ class DataLoader(object):
         df = df.loc[
             df["price_gbp"] <= self.max_price
         ]  # not interested in prices over 100 MM
-        self._raw_df = df.loc[df["is_london_borough"]]
-        LOGGER.debug(self._raw_df["address_county_1"].unique())
         LOGGER.debug("filtered to london only")
+        return df.loc[df["is_london_borough"]]
 
-    def _aggregate_data(self):
-        """
-        Aggregates the data to have an index of year, month, london borough and value price gbp --> mean or count
+    def _aggregate_data(self) -> Tuple[pandas.DataFrame, pandas.DataFrame]:
+        """Get the borough df and aggregated df.
 
-        :return:
+        Performs group bys to produce much smaller datasets required for the plots.
+        :return: tuple of pandas Dataframes that have been aggregated.
         """
-        df = self._raw_df.groupby(["year", "month", "address_county_1"]).aggregate(
+        borough_df = self._raw_df.groupby(["year", "month", "address_county_1"]).aggregate(
             {"price_gbp": ["mean", "median", "count"], "date_time": "first"}
         )
-        df.columns = ["_".join(col) for col in df.columns]
-        df = df.dropna(subset=["price_gbp_count"])
-        df["date_time_first"] = df["date_time_first"].values.astype("datetime64[M]")
-        df = df.rename(columns={"date_time_first": "date_time"})
+        borough_df.columns = ["_".join(col) for col in borough_df.columns]
+        borough_df = borough_df.dropna(subset=["price_gbp_count"])
+        borough_df["date_time_first"] = borough_df["date_time_first"].values.astype("datetime64[M]")
+        borough_df = borough_df.rename(columns={"date_time_first": "date_time"})
 
         aggregated_data = self._raw_df.groupby(["year", "month"]).aggregate(
             {"price_gbp": ["mean", "median", "count"], "date_time": "first"}
@@ -183,58 +193,61 @@ class DataLoader(object):
         aggregated_data = aggregated_data.rename(
             columns={"date_time_first": "date_time"}
         )
+        return borough_df, aggregated_data
 
-        self._borough_data = df
-        self._aggregated_data = aggregated_data
 
-    def load_prepare_and_aggregate_data(self):
-        """
-        Loads prepares and aggregates the self._data attribute
 
-        :return:
+    def load_prepare_and_aggregate_data(self) -> None:
+        """Loads prepares and aggregates the self._data attribute.
+
+        Updates the dataframe attributes of the object.
         """
         if self._cached_data_available():
-            self._load_cached_data()
+            self._borough_data, self._aggregated_data = self._load_cached_data()
         else:
-            self._load_data(self.default_data_path)
-            self._update_data_for_london_analysis()
-            self._aggregate_data()
+            self._raw_df = self._load_data(self.default_data_path)
+            self._raw_df = self._update_data_for_london_analysis()
+            self._borough_data, self._aggregated_data = self._aggregate_data()
             self._save_data_to_disk()
 
-    def _cached_data_available(self):
+    def _cached_data_available(self) -> bool:
+        """Checks if the cached data is available."""
         return os.path.exists(self._cached_data_path) and os.path.exists(
             self._cached_yearly_data_path
         )
 
-    def get_mean_prices(self, year, month):
+    def get_mean_prices(self, year:int, month:int) -> pandas.Series:
+        """Get the mean price for that year, month as a series for all address_county_1s"""
         LOGGER.debug("getting mean price for (%s, %s)", year, month)
         return self._borough_data.loc[(year, month)][("price_gbp_mean")]
 
-    def get_median_prices(self, year, month):
+    def get_median_prices(self, year: int, month: int) -> pandas.Series:
+        """Get the median price for that year, month as a series for all address_county_1s"""
         LOGGER.debug("getting mean price for (%s, %s)", year, month)
         return self._borough_data.loc[(year, month)][("price_gbp_median")]
 
-    def _save_data_to_disk(self):
-        """
-        save data to disk
-
-        :return:
-        """
+    def _save_data_to_disk(self)->None:
+        """Save the borough and aggregated dataframes to disk for caching."""
         self._borough_data.to_csv(self._cached_data_path, index=True)
         self._aggregated_data.to_csv(self._cached_yearly_data_path, index=True)
 
-    def _load_cached_data(self):
+    def _load_cached_data(self) -> Tuple[pandas.DataFrame, pandas.DataFrame]:
+        """Loads cached borough and aggregated dataframe files and loads them.
+
+        :return: Tuple of the borough_df and aggregated_df
         """
-        Loads the data
-        :return:
-        """
-        self._borough_data = pd.read_csv(self._cached_data_path, index_col=[0, 1, 2])
-        self._aggregated_data = pd.read_csv(
+        borough_data = pandas.read_csv(self._cached_data_path, index_col=[0, 1, 2])
+        aggregated_data = pandas.read_csv(
             self._cached_yearly_data_path, index_col=[0, 1], parse_dates=["date_time"]
         )
-        self._raw_data = None
+        return borough_data, aggregated_data
 
-    def get_line_data(self):
+
+    def get_line_data(self) -> Tuple[numpy.ndarray, numpy.ndarray]:
+        """Gets the data necessary for the median line data below the cmap plot.
+
+        :return: Tuple of the x (datetime) and y (median price) data for the plot.
+        """
         x_data, y_data = (
             self._aggregated_data["date_time"],
             self._aggregated_data["price_gbp_median"],
--- borough_map/map_view.py
+++ borough_map/map_view.py
@@ -1,20 +1,29 @@
-import shapefile as shp
-import matplotlib.pyplot as plt
 import os
-import numpy as np
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-import matplotlib
+from typing import Optional
+
+import shapefile as shp
+import numpy
+import pandas
+
 import matplotlib.animation
 import matplotlib.ticker
-import pandas
+import matplotlib.pyplot as plt
+
 from pandas.plotting import register_matplotlib_converters
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
 
 register_matplotlib_converters()
 
 
-class MapView(object):
-    def __init__(self, path=None):
+class MapView:
+    """Class to plot the view (map and line plots etc.)."""
+
+    def __init__(self, path:Optional[str]=None):
+        """Instantiate the MapView.
+
+        :param path:
+        """
 
         if path is None:
             self.shp_path = os.path.join(
@@ -30,7 +39,6 @@ class MapView(object):
         self.map_ax = self.fig.add_subplot(gs[0:2, :])
         self.plot_ax = self.fig.add_subplot(gs[2, :])
 
-        # self.fig, (self.map_ax, self.plot_ax) = plt.subplots(nrows=2)
         self.borough_to_plot_dict = {}
         self.boroughs = []
         self.patches = []
@@ -41,30 +49,28 @@ class MapView(object):
             "WESTMINSTER": "CITY OF WESTMINSTER"
         }  # align between names in house price data and in shape file
 
-    def initial_draw(self):
-        """
-        Initially draw all plots on the map
-
-        :return:
-        """
+    def initial_draw(self) -> None:
+        """Initial configuration and drawing of plots (called once at beginning of animation)."""
         self._loop_over_shape_file_and_create_polygons()
         self.sort_patches_and_boroughs()
         self._add_patches_to_collection_and_axis()
         self._configure_axis()
         self._create_initial_color_bar()
 
-    def _loop_over_shape_file_and_create_polygons(self):
+    def _loop_over_shape_file_and_create_polygons(self) -> None:
+        """Loops over the records in the shape files (boroughs) and appends them to lists for drawing."""
         for shape in self.shape_reader.shapeRecords():
             borough = shape.record[0].upper()
             if borough in self._borough_name_mappings:
                 borough = self._borough_name_mappings[borough]
-            xy = np.array(shape.shape.points)
+            xy = numpy.array(shape.shape.points)
             polygon = Polygon(xy, False)
             self.patches.append(polygon)
             self.borough_to_plot_dict[borough] = polygon
             self.boroughs.append(borough)
 
-    def _configure_axis(self):
+    def _configure_axis(self) -> None:
+        """Configure the plot axes."""
         self.map_ax.set_xlim(left=5e5, right=5.6e5)
         self.map_ax.set_ylim(bottom=1.5e5, top=2.2e5)
         self.map_ax.set_aspect("equal")
@@ -80,31 +86,34 @@ class MapView(object):
             matplotlib.ticker.FuncFormatter(lambda x, p: "{}k".format(int(x // 1000)))
         )
 
-    def _add_patches_to_collection_and_axis(self):
+    def _add_patches_to_collection_and_axis(self) -> None:
+        """Add the patches (borough boundaries) to a PatchCollection and attach to the axes plot."""
         self.patch_collection = PatchCollection(self.patches, cmap="plasma")
         self.map_ax.add_collection(self.patch_collection)
 
-    def sort_patches_and_boroughs(self):
+    def sort_patches_and_boroughs(self) -> None:
+        """Make sure patches (boroughs) are sorted alphebetically so they match DataLoader's order."""
         zipped = zip(self.boroughs, self.patches)
         self.patches = [
             patch for borough, patch in sorted(zipped, key=lambda pair: pair[0])
         ]
         self.boroughs = sorted(self.boroughs)
 
-    def _create_initial_color_bar(self):
-        array = np.array((len(self.patches) - 1) * [0] + [1e6])
+    def _create_initial_color_bar(self) -> None:
+        """Create a colour bar for the cmap plot"""
+        array = numpy.array((len(self.patches) - 1) * [0] + [1e6])
         self.patch_collection.set_array(array)
         colorbar = self.fig.colorbar(self.patch_collection, ax=self.map_ax)
         self.patch_collection.set_clim(0, 1e6)
         colorbar.vmin = 0
         colorbar.vmax = 1e6
-        colorbar.set_ticks(np.arange(0, 1.1e6, 1e5))
+        colorbar.set_ticks(numpy.arange(0, 1.1e6, 1e5))
         colorbar.ax.yaxis.set_major_formatter(
             matplotlib.ticker.FuncFormatter(lambda x, p: "{}k".format(int(x // 1000)))
         )
 
-    def draw_year_month_on_axis(self, year, month):
-        """
+    def draw_text_on_axis(self, year: int, month: int)->None:
+        """Write the year month as text on the axes and disclaimer text.
 
         :param year:
         :param month:
@@ -132,18 +141,22 @@ class MapView(object):
             fontsize=4,
         )
 
-    def show(self):
+    def show(self) -> None:
         plt.show()
 
-    def set_colors_for_patches(self, colors_array):
-        """
+    def set_colors_for_patches(self, colors_array: numpy.ndarray) -> None:
+        """Set the values to each patch (gets mapped to color in cmap plot).
 
-        :param np.array colors_array: same length as self.patches
-        :return:
+        :param numpy.array colors_array: same length as self.patches
         """
         self.patch_collection.set_array(colors_array)
 
-    def animate(self, update_function, frames):
+    def animate(self, update_function, frames) -> None:
+        """Animate the plot using update_function for a specified number of frames.
+
+        :param update_function: function to be called to update map view (using updated data)
+        :param frames:  number of frames
+        """
         Writer = matplotlib.animation.writers["ffmpeg"]
         writer = Writer(fps=15, metadata=dict(artist="Tim"))
 
@@ -156,9 +169,9 @@ class MapView(object):
             repeat=False,
         )
         self.animation.save("mean_prices.mp4", writer=writer)
-        # plt.show()
 
-    def plot_line(self, plot_x_data, plot_y_data):
+    def plot_line(self, plot_x_data: numpy.ndarray, plot_y_data: numpy.ndarray):
+        """Update the line plot."""
         self.line.set_data(plot_x_data, plot_y_data)
 
 
